3c826ed9f874cfe9cfe31bb18d4b2f20b9bdab8b
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-01-04 20:43:44 +0530

    Time: 415 ms (37.03%) | Memory: 67.1 MB (72.58%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    void merge(vector<int>& arr,vector<int>&temp , int start, int mid, int end){
-        int i = start,  j=mid+1,  k = start;
-        while(i<=mid && j<=end){
-            if(arr[i]<=arr[j]){
-                temp[k++]=arr[i++];
-            }
-            else{
-                temp[k++]=arr[j++];
-            }
-        }
-        while(i<=mid){
-            temp[k++]=arr[i++];
-        }
-        while(j<=end){
-            temp[k++]=arr[j++];
-        }
-
-        while(start<=end){
-            arr[start] = temp[start];
-            ++start;
-        }
+    void mergeInPlace(vector<int>& arr, int start, int mid, int end){
+       int total_len = end-start+1;
+       int gap = (total_len/2) + (total_len%2);
+       while(gap>0){
+           int i = start, j= start+gap;
+           while(j<=end){
+               if(arr[i]>arr[j]){
+                   swap(arr[i],arr[j]);
+               }
+               i++,j++;
+           }
+           if(gap>1){
+           gap= (gap/2)+(gap%2);
+           }
+           else{
+               gap=0; //because after gap becomes 1 , if we apply formula for updation of gap, we will get 1 again. Therefore we will update gap as 0 once gap=1 is used.
+           }
+       }
     }
 
-    void mergeSort(vector<int>& arr,vector<int>&temp , int start, int end){
+    void mergeSort(vector<int>& arr, int start, int end){
         if(start>=end){
             return;
         }
         // int mid = start + (end-start)/2;
         int mid = (start+end)>>1;
-        mergeSort(arr,temp,start,mid);
-        mergeSort(arr,temp,mid+1, end);
-        merge(arr,temp,start,mid,end);
+        mergeSort(arr,start,mid);
+        mergeSort(arr,mid+1, end);
+        mergeInPlace(arr,start,mid,end);
     }
     
     vector<int> sortArray(vector<int>& arr) {
-        vector<int>temp(arr.size(),0);
-        mergeSort(arr,temp,0,arr.size()-1);
+        mergeSort(arr,0,arr.size()-1);
         return arr;
     }
 };
