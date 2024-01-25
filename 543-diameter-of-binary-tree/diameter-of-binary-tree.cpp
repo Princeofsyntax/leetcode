7dfc65cc85c98198d316464b06cc31107e7adaf8
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-01-25 14:40:59 +0530

    Time: 14 ms (24.65%) | Memory: 18.9 MB (98.95%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,34 +11,21 @@
  */
 class Solution {
 public:
+    int D = 0;
     
-int height(TreeNode* root){
-    //base case
-        if(root == NULL){
-            return 0;
-        }
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        int ans = max(leftHeight , rightHeight) +1 ;//+1 is done to consider root node also
+    int height(TreeNode* root){
+        if(!root) return 0;
 
-        return ans;
-
-}
+        int lh = height(root->left);
+        int rh = height(root->right);
+        //Diameter 
+        int currD = lh+rh;
+        D = max(D,currD);
+        return max(lh,rh) + 1;
+    }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-
-        //option1
-        int op1 = diameterOfBinaryTree(root->left);
-        //option2
-        int op2 = diameterOfBinaryTree(root->right);
-        //option 3(main)
-        int op3 = height(root->left) +  height(root->right); // we have to count no. of edges
-        int ans = max(op1, max(op2,op3));
-
-        return ans;
-
+       height(root);
+       return D;
     }
 };
