41026d991d434fffd5403f8bc88d7d338ac1e364
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-03-11 15:36:17 +0530

    Time: 19 ms (64.43%) | Memory: 20.1 MB (16.26%) - LeetSync

---
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        int stops = 0;
+        int dist = 0;
+        int i = 0;
+        int fuel = startFuel;
+        priority_queue<pair<int,int>>pq; //maxHeap --> {fuel, position}
+        while(true){
+          while(i < stations.size()){
+              //push stations within the reach with my current fuel level from my current position
+              if(stations[i][0] <= dist+fuel){
+                  pq.push({stations[i][1], stations[i][0]});
+              }
+              else{
+                  break;
+              }
+              ++i;
+          }
+          
+          //travel with full fuel level
+          dist += fuel;
+          fuel = 0;
+          //reached
+          if(dist >= target) break;
+
+           //no fuel stations within reach
+           if(pq.empty()){
+               stops = -1;
+               break;
+           }
+          
+          //refuel and re-adjust dist and fuel based on the distance travelled and fuel used till that station
+          auto &top = pq.top();
+          fuel = (dist - top.second) + top.first;
+          dist =  top.second;
+          pq.pop();
+          ++stops;
+        }
+
+        return stops;
+    }
+};
