1b57ba43081b4f2a7791dcacaaabbbc61901a2cd
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-03-11 12:28:03 +0530

    Time: 211 ms (31.76%) | Memory: 152.3 MB (25.20%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; //so that push/pop can be done from front and back
-        vector<int> ans;
+        priority_queue<pair<int,int>>pq; // < value, index >
+        vector<int>ans;
 
-        //first window of k size
-        for(int i = 0; i<k; i++){
-            //chote element remove krdo
-            while( !dq.empty() && nums[i] >= nums[dq.back()])
-            {
-                dq.pop_back();
-            }
-            //inserting index, so that we can check out of window element
-            dq.push_back(i);
+        //consider first window
+        for(int i = 0; i < k; i++){
+            pq.push({ nums[i],i });
         }
 
-        //store answer for first window
-        ans.push_back(nums[dq.front()]);
+        //store first window max
+        ans.push_back(pq.top().first);
 
-        //remaining window process; one one step
-        for(int i = k; i<nums.size(); i++)
-        {
-            //remove out of window element
-            if( !dq.empty() && i - dq.front() >= k)
-            {
-                dq.pop_front();
+        //consider rest of the windows
+        for(int i = k; i< nums.size(); i++){
+            pq.push({nums[i],i});
+
+            //remove if max is from previous window
+            while( pq.top().second <= i-k){
+                pq.pop();
             }
-            //again chote element remove krdo
-            while( !dq.empty() && nums[i] >= nums[dq.back()])
-            {
-                dq.pop_back();
-            }
-            //inserting index, so that we can check out of window element
-            dq.push_back(i);
-            
-            //store current window answer
-            ans.push_back(nums[dq.front()]);
+
+            ans.push_back(pq.top().first);
         }
-       return ans;
+        return ans;
     }
 };
