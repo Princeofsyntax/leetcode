48016fb69bf5721d5363c25b6d45626013381805
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-03-22 02:23:05 +0530

    Time: 10 ms (91.60%) | Memory: 15.7 MB (9.75%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+      int solveOptimal(vector<int> &arr, vector<int> &lis)
+    {
+        if (arr.size() == 0)
+            return 0;
+        vector<int> ans;
+
+        lis.push_back(1); // as current element will be counted in every case
+
+        ans.push_back(arr[0]);
+
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i] > ans.back())
+            {
+                // include
+                ans.push_back(arr[i]);
+                lis.push_back(ans.size());
+            }
+            else
+            {
+                int index = lower_bound(ans.begin(), ans.end(), arr[i]) - ans.begin(); // provides the required index
+                ans[index] = arr[i];
+                lis.push_back(index + 1);
+            }
+        }
+        return ans.size();
+    }
+
+    int minimumMountainRemovals(vector<int> &nums)
+    {
+        vector<int> lis, lds; // longest increasing subsequence and longest decreasing subsequence
+        // make lis and lds array by taking each element as pivot
+        solveOptimal(nums, lis);
+        // reverse to get lds
+        reverse(nums.begin(), nums.end());
+        solveOptimal(nums, lds);
+
+        // main logic
+        int largestMountainLength = INT_MIN;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (lis[i] == 1 || lds[nums.size() - i - 1] == 1)
+            {
+                // as lds was reversed
+                // 1 means not forming mountain
+                continue;
+            }
+            largestMountainLength = max(largestMountainLength, lis[i] + lds[nums.size() - i - 1] - 1);
+        }
+        int minRemovals = nums.size() - largestMountainLength;
+        return minRemovals;
+    }
+};
