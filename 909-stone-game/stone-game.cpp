a58ccefc8127a59fe8f7f51498d0bf5291f08444
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-03-22 16:46:45 +0530

    Time: 5 ms (38.82%) | Memory: 9.3 MB (58.91%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
+       // There will be atleast 1 way by which alice will always win --> and problem is asking is there any way --> so YES
         return true;
     }
 };
