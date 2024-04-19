d7a471d502f592aebb9cc678fca844eeca9ac665
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-04-20 02:11:42 +0530

    Time: 8 ms (79.44%) | Memory: 13.3 MB (49.44%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
+    static bool cmp(pair<int,int>a, pair<int,int>b){
+      return a.first < b.first; 
+    } 
+
     vector<int> twoSum(vector<int>& nums, int target) {
-         for (int i = 0; i < nums.size()-1; i++)
-    {
-        //for every element , will traverse on aage waale elements
-        for (int j = i+1; j < nums.size() ; j++)
-        {
-            
-            if (nums[i] + nums[j] == target)
-            {
-               return {i,j};
-            }
-            
-        } 
-    }
-    //not found
-    return{};
+      //First store the indices corresponding to its value so that we can do sorting without loss of indices
+      //Sort the array and then use 2 pointer approach
+      vector<pair<int,int>>data;
+      for(int i = 0; i < nums.size(); i++)
+      {
+        int value = nums[i];
+        int index = i;
+        data.push_back({value,index});
+      }
+
+      sort(data.begin(), data.end(), cmp); //sorting w.r.t value
+
+      int start = 0;
+      int end = data.size()-1;
+      vector<int>ans;
+
+      while(start < end)
+       {
+         if(data[start].first + data[end].first == target)
+         {
+            ans.push_back(data[start].second); 
+            ans.push_back(data[end].second); 
+            break;
+         }
+         else if(data[start].first + data[end].first > target)
+         {
+            end--;
+         }
+         else
+         {
+            start++;
+         }
+      }
+      return ans;
     }
 };
