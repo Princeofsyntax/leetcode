afc094c0fd4367b3b89407b404a2d93e5108e2fd
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-04-24 20:23:16 +0530

    Time: 107 ms (63.06%) | Memory: 59.4 MB (81.60%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& land, int i, int j, int &x, int &y){
+        //x and y will provide coordinate for bottom right and i,j will provide coordinate for top left
+        //base case
+        if(i < 0 || j < 0 || i == land.size() || j == land[0].size() ){
+           return;
+        } 
+        if(land[i][j] != 1) return;
+        
+        land[i][j] = 2; //mark as visited
+        //modify the x and y coordinates
+        x = max(x, i);
+        y = max(y, j);
+
+        //recursion
+        dfs(land, i+1, j,x, y);
+        dfs(land, i, j+1,x, y);
+    }
+
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        vector<vector<int>>ans;
+
+        for(int i = 0; i < land.size(); i++){
+            for(int j = 0; j < land[0].size(); j++){
+                if(land[i][j] == 1){
+                    int x = i;
+                    int y = j;
+                    dfs(land, i, j, x, y);
+                    ans.push_back({i, j, x, y});
+                }
+            }
+        }
+        return ans;
+    }
+};
