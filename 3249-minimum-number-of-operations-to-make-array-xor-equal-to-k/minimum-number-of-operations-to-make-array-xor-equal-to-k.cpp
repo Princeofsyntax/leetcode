8bb1211814cd504646e4b155bbfe89f82d96f4f5
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-04-29 12:14:09 +0530

    Time: 96 ms (92.32%) | Memory: 91.4 MB (54.80%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,7 +1,33 @@
 class Solution {
  public:
-  int minOperations(vector<int>& nums, int k) {
-    const int xors = accumulate(nums.begin(), nums.end(), 0, bit_xor<>());
-    return __builtin_popcount(k ^ xors);
+  int countNumberOfSetBits(int n){
+    int count = 0;
+    while(n != 0){
+        int lastBit = n & 1;
+        if(lastBit) count++;
+        //right shift
+        n = n>>1;
+    }
+    return count;
   }
+
+   int minOperations(vector<int>& nums, int k) {
+        if (nums.size() < 1) // Handle the case where nums is empty
+            return -1; // Or whatever value you want to return for this case
+
+        //Firstly we will take xor of all the numbers in given vector and store in variable xor_calc
+        //Then we will do xor of xor_calc with given k and store result in k_calc
+        // now we will count number of set bits in k_calc to get answer
+        int xor_calc = 0; // Initialize with 0
+
+        for (int i = 0; i < nums.size(); i++) {
+            xor_calc = xor_calc ^ nums[i];
+        }
+
+        int k_calc = k ^ xor_calc;
+
+        int ans = countNumberOfSetBits(k_calc);
+
+        return ans;
+    }
 };
