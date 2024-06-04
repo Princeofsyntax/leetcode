f5a85a0271357bd3c43c7bdd254b7a542accd7b5
Princeofsyntax <142150438+Princeofsyntax@users.noreply.github.com>
2024-06-05 00:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.42%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int charCount[128] = {};
+
+        for (char c : s) {
+            charCount[c]++;
+        }
+
+        int maxLength = 0; 
+
+        for (int count : charCount) {
+            maxLength += count - (count % 2);
+
+           if (maxLength % 2 == 0 && count % 2 == 1) {
+                maxLength++;
+            }
+        }
+
+       return maxLength;
+    }
+};
