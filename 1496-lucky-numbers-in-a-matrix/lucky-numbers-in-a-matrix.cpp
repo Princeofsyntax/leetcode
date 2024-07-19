65cb12f529c2c355be442300b97216fcf410d8a9
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-07-19 11:45:14 +0530

    Time: 18 ms (30.19%) | Memory: 14.5 MB (12.50%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -2,39 +2,30 @@ class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int n = matrix.size();
+        if (n == 0) return {};
         int m = matrix[0].size();
-        if(n == 0) return {};
-
-        vector<int>minInRow(n);
-        vector<int>maxInCol(m);
-
-        int minRow = INT_MAX;
-        int maxCol = INT_MIN;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                 if(matrix[i][j] < minRow){
-                    minRow = matrix[i][j];
-                 }
-            }
-            minInRow[i] = minRow;
-            minRow = INT_MAX;
-        }
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                 if(matrix[j][i] > maxCol){
-                    maxCol = matrix[j][i];
-                 }
-            }
-            maxInCol[i] = maxCol;
-            maxCol = INT_MIN;
-        }
         
-        vector<int>ans;
+        vector<int> minInRow(n, INT_MAX);
+        vector<int> maxInCol(m, INT_MIN);
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(matrix[i][j] == minInRow[i] && matrix[i][j] == maxInCol[j]){
+        // Find the minimum in each row and the maximum in each column in a single pass
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] < minInRow[i]) {
+                    minInRow[i] = matrix[i][j];
+                }
+                if (matrix[i][j] > maxInCol[j]) {
+                    maxInCol[j] = matrix[i][j];
+                }
+            }
+        }
+
+        vector<int> ans;
+
+        // Find the lucky numbers
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == minInRow[i] && matrix[i][j] == maxInCol[j]) {
                     ans.push_back(matrix[i][j]);
                 }
             }
