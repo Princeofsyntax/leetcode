a4c6301efb2f62f9822ffe55a475ac401ad03008
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-09-02 00:52:14 +0530

    Time: 76 ms (60.53%) | Memory: 70.6 MB (20.52%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    int maxSumArrayHelper(vector<int>&arr,int start,int end){
-        //base case
-        if(start == end){
-            return arr[start];  //last level of DnC
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN, sum = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            
+            if(sum > maxi){
+                maxi = sum;
+            }
+
+            if(sum < 0){
+                sum = 0;
+            }
         }
-        //when recursive tree is formed then when we take left branch sum then it is leftBorderSum and when we take right
-        //it is called rightBorderSum, we also take middle sum by considering the root element as well-->maxCrossBorderSum
-        int maxLeftBorderSum=INT_MIN, maxRightBorderSum=INT_MIN;
-        int mid = start + ((end-start)>>1);
-        
-        //dividing
-        int maxLeftSum=maxSumArrayHelper(arr,start,mid);
-        int maxRightSum=maxSumArrayHelper(arr,mid+1,end);
 
-        //maxCrossBorderSum
-        //LeftSum
-         int leftBorderSum=0, rightBorderSum=0;
-         for(int i = mid; i>=start; i--){
-             leftBorderSum += arr[i];
-             if(leftBorderSum > maxLeftBorderSum){
-                 maxLeftBorderSum = leftBorderSum;
-             }
-         }
-         //RightSum
-         for(int i = mid+1; i<=end; i++){
-             rightBorderSum += arr[i];
-             if(rightBorderSum > maxRightBorderSum){
-                 maxRightBorderSum = rightBorderSum;
-             }
-         }
-         int crossBorderSum =  maxRightBorderSum +  maxLeftBorderSum ;
-         return max(maxLeftSum, max(maxRightSum, crossBorderSum));
-
-    }
-    
-    int maxSubArray(vector<int>& arr) {
-        return maxSumArrayHelper(arr,0,arr.size()-1);
+        return maxi;
     }
 };
