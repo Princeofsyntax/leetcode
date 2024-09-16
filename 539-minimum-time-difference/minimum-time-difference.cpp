0157898a46dd9460b202ac6295eaa0b14b93d443
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-09-16 05:58:03 +0530

    Time: 13 ms (53.24%) | Memory: 17.7 MB (60.05%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        // first convert in minutes
-    vector<int> minutes;
-    for (int i = 0; i < timePoints.size(); i++)
-    {
-        string curr = timePoints[i];
-        // hours are in string format
-        int hours = stoi(curr.substr(0, 2));
-        // stoi converts string to integer
-        int minute = stoi(curr.substr(3, 2));
-        int totalMinutes = hours * 60 + minute;
-        minutes.push_back(totalMinutes);
-    }
+        vector<int>minutes;
+        for(const auto& time : timePoints){
+            int hours = stoi(time.substr(0,2));
+            int minute = stoi(time.substr(3,2));
+            int totalMin = hours*60 + minute;
+            minutes.push_back(totalMin);
+        }
+        sort(minutes.begin(), minutes.end());
+        int minimum = INT_MAX;
+        int n = minutes.size();
+        for(int i = 0; i < n-1; i++){
+            minimum = min(minimum, minutes[i+1]-minutes[i]);
+        }
+        //last check
+        minimum = min(minimum, (minutes[0]+1440) - minutes[n-1]);
 
-    // sort to deccrease comparison --> so that we have to check adjacent only
-    sort(minutes.begin(), minutes.end());
-
-    // find difference
-    int mini = INT_MAX;
-    int n = minutes.size();
-
-    for (int i = 0; i < n - 1; i++) // n-1 is taken as we are already using i+1
-    {
-        int diff = minutes[i + 1] - minutes[i];
-        mini = min(mini, diff);
-    }
-
-    // something missing
-    int lastDiff = (minutes[0] + 1440) - minutes[n - 1];
-    mini = min(mini, lastDiff);
-
-    return mini;
+        return minimum;
     }
 };
