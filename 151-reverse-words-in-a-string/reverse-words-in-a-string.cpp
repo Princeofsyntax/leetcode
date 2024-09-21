e00410003273a3f79392127c41f15a0727f91f63
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-09-21 19:19:27 +0530

    Time: 3 ms (81.20%) | Memory: 10.2 MB (51.92%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        if (s.size() == 0) return s;
-
-        vector<string> helper;
-        string toAdd = "";
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != ' ') {
-                toAdd.push_back(s[i]);
-            } else if (s[i] == ' ' && toAdd.size() > 0) {
-                helper.push_back(toAdd);
-                toAdd = "";
-            }
-        }
-
-        // Add the last word if it exists
-        if (toAdd.size() > 0) {
-            helper.push_back(toAdd);
-        }
-
+        stringstream st(s);
+        string word;
         string ans = "";
-        for (int i = helper.size() - 1; i >= 0; i--) {
-            ans += helper[i];
-            if (i > 0) {
-                ans += " ";
-            }
+        stack<string>helper;
+        while(st >> word){
+            helper.push(word);
         }
-
+        while(!helper.empty()){
+            ans+=helper.top() + " ";
+            helper.pop();
+        }
+        if(!ans.empty()){
+            ans.pop_back();
+        }
         return ans;
     }
 };
