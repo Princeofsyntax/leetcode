693cd5eaea7ab049ffeeb7c0d61105f162829a7f
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-10-08 15:23:52 +0530

    Time: 100 ms (73.84%) | Memory: 32.5 MB (91.34%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int balance = 0;  
+        int maxUnbalanced = 0;  
+
+        for (char c : s) {
+            if (c == '[') {
+                balance++;  
+            } else {
+                balance--; 
+            }
+            if (balance < 0) {
+                maxUnbalanced = max(maxUnbalanced, -balance);
+            }
+        }
+
+        return (maxUnbalanced + 1) / 2;
+    }
+};
