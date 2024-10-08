8f78209b67401979127518bbff21fafe58517aa3
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-10-09 04:21:35 +0530

    Time: 84 ms (53.80%) | Memory: 13 MB (84.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
-        int solveUsingOptimization(vector<int> &arr, int curr, int prev)
-        {
-            int n = arr.size();
-            vector<int> currRow(n + 1, 0);
-            vector<int> nextRow(n + 1, 0);
-
-            // move opposite to the top down approach
-            for (int curr = n - 1; curr >= 0; curr--)
-            {
-                for (int prev = curr - 1; prev >= -1; prev--)
-                {
-                    // include
-                    int include = 0;
-                    if (prev == -1 || arr[curr] > arr[prev])
-                    {
-                        include = 1 + nextRow[curr + 1]; // prev+1 is used to manage when prev becomes -1
-                    }
-                    // exclude
-                    int exclude = 0 + nextRow[prev + 1]; // prev+1 is used to manage when prev becomes -1
-
-                    currRow[prev + 1] = max(include, exclude);
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
                 }
-                // shifting
-                nextRow = currRow; // as movement is opposite
             }
-            return nextRow[0];
         }
-
-    int lengthOfLIS(vector<int> &nums)
-        {
-            int curr = 0;  // index of current element
-            int prev = -1; // index of last element included in subsequence
-            int n = nums.size();
-            int ans = solveUsingOptimization(nums, curr, prev);
-            return ans;
-        }
-
+        return *max_element(dp.begin(), dp.end());
+    }
 };
