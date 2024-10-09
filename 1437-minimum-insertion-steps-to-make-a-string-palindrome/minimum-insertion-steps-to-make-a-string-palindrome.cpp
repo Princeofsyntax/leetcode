8451c2e6611be805d23062c55afa40c0e60bdf5f
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-10-09 06:10:55 +0530

    Time: 60 ms (8.89%) | Memory: 29.9 MB (56.62%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
-      //refer leetcode 516 code
-      int solveUsingTabSO(string a, string b)
-    {
-        // vector<vector<int>>dp (a.length() + 10, vector<int>(b.length()+10,0));
-        vector<int> curr(b.length() + 1, 0);
-        vector<int> next(b.length() + 1, 0);
+    int f(int i, int j, string & s, string & t, vector<vector<int>>&dp){
+        if(i < 0 || j < 0)return 0;
+        if(dp[i][j] != -1)return dp[i][j];
 
-        for (int i = a.length() - 1; i >= 0; i--)
-        {
-            for (int j = b.length() - 1; j >= 0; j--)
-            {
-                int ans = 0;
-                if (a[i] == b[j])
-                    ans = 1 + next[j + 1];
-                else
-                    ans = 0 + max(curr[j + 1], next[j]);
-
-                curr[j] = ans;
-            }
-            // shift
-            next = curr;
-        }
-        return next[0];
+        if(s[i] == t[j]) return dp[i][j] =  1 + f(i-1, j-1, s, t,dp);
+        return dp[i][j] =  max(f(i-1,j,s,t,dp), f(i,j-1,s,t,dp));
     }
 
-    int longestPalindromeSubseq(string &s)
-    {
-        string first = s;
-        reverse(s.begin(), s.end());
-        string second = s;
-        return solveUsingTabSO(first, second);
+    int minInsertions(string s) {
+        string t = s;  
+        reverse(t.begin(), t.end());  
+        int n = s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int len = f(n-1, n-1, s, t,dp);
+        return n-len;
     }
-
-    int minInsertions(string s)
-    {
-        // length of subsequence which is already palindrome
-        int LPS_len = longestPalindromeSubseq(s);
-        int insertionsToDo = s.size() - LPS_len;
-        return insertionsToDo;
-    }
-
 };
