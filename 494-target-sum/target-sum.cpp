8d111a368426f2f0657ef7547f19fa538f61d2d9
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-10-24 18:18:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (60.31%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-      int solve(vector<int> &nums, int target)
-    {
-        // create map
-        map<pair<int, int>, int> dp;
-        dp[{nums.size(), 0}] = 1; // base case
-
-        // target value also can go to negative --> (target - sum of all elements) = target range decider
-        int total = 0;
-        for (auto num : nums)
-        {
-            total += num; // total sum
+    int helper(int i, vector<int>&arr, int sum, vector<vector<int>>& dp) {
+        if (i == 0) {
+            if(arr[0] == 0 && sum == 0) return 2; 
+            if(sum == 0 || sum == arr[0])return 1;
+            return 0;
+        } 
+        
+        
+        if (dp[i][sum] != -1) return dp[i][sum];
+        
+        int notPick = helper(i - 1, arr, sum, dp);
+        
+        int pick = 0;
+        if (arr[i] <= sum) {
+            pick = helper(i - 1, arr, sum - arr[i], dp);
         }
-
-        for (int i = nums.size() - 1; i >= 0; i--)
-        {
-            // range of target --> -total to +total
-            for (int sum = -total; sum <= total; sum++)
-            {
-                int plus = dp.find({i + 1, sum - nums[i]}) != dp.end() ? dp[{i + 1, sum - nums[i]}] : 0;
-                int minus = dp.find({i + 1, sum + nums[i]}) != dp.end() ? dp[{i + 1, sum + nums[i]}] : 0;
-
-                // store
-                dp[{i, sum}] = plus + minus;
-            }
-        }
-        return dp[{0, target}];
+        
+        return dp[i][sum] = (pick + notPick);
     }
+    
 
-    int findTargetSumWays(vector<int> &nums, int target)
-    {
-        return solve(nums, target);
+    int findTargetSumWays(vector<int>& arr, int d) {
+        // basically we have to divide the nums into two parts such that there difference is target
+         // Target is to find a sum = (totalSum - d)/2 
+        int n = arr.size();
+        int totalSum = accumulate(arr.begin(), arr.end(), 0);
+        if(totalSum - d < 0 || (totalSum - d)%2 ) return false; // As we need even
+        int sum = (totalSum - d)/2;
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return helper(n-1, arr, sum, dp);
     }
-
 };
