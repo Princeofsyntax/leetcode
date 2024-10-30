20c987f17abb41b86433fb657d63c5e2651c2882
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-10-31 00:28:34 +0530

    Time: 39 ms (26.68%) | Memory: 25.7 MB (59.68%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    int solve(string &s, string &t)
-    {
-        // create 2D dp
-        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
-        // handle base case
-        for (int i = 0; i <= s.size(); i++)
-        {
-            dp[i][t.size()] = 1;
-        }
+    int helper(int i, int j, string &s, string &t, vector<vector<int>>&dp){
+        if(j == 0)return 1;
+        if(i == 0)return 0;
+        if(dp[i][j] != -1)return dp[i][j];
 
-        // proceed
-        for (int i = s.size() - 1; i >= 0; i--)
-        {
-            for (int j = t.size() - 1; j >= 0; j--)
-            {
-                long long ans = 0;
-                if (s[i] == t[j])
-                {
-                    // consider the current character
-                    ans += dp[i + 1][j + 1];
-                }
-                // neglect the current character
-                ans += dp[i + 1][j];
-
-                // store and return
-                dp[i][j] = ans;
-            }
-        }
-        return dp[0][0];
+        if(s[i-1] == t[j-1]) return dp[i][j] = helper(i-1, j-1, s, t, dp) + helper(i-1, j, s, t, dp);
+        return dp[i][j] = helper(i-1, j, s, t, dp);
     }
 
-    int numDistinct(string s, string t)
-    {
-        return solve(s, t);
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        // convert in 1 based indexing
+        vector<vector<int>>dp(n+1,vector<int>(m+1, -1));
+        return helper(n, m, s, t, dp);
     }
 };
