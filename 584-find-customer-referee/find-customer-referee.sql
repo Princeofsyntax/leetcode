cafa5852aab1c6e0b346004c4c2b1ed5550e98b2
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-11-25 01:04:02 +0530

    Time: 628 ms (21.14%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
 SELECT name 
 FROM Customer
-WHERE referee_id != 2 OR referee_id IS NULL;
+-- WHERE referee_id != 2 OR referee_id IS NULL; // --> will also work
+WHERE 
+    IFNULL(referee_id, 0) <> 2; # means change refree_id to 0 where it is null (by default sql do not take null as output so these are required)
+    
