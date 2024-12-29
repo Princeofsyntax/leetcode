527f77b504325310dc56c1635ad9b8bf168b6a0e
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-12-29 19:33:02 +0530

    Time: 558 ms (16.69%) | Memory: 296.6 MB (5.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+    int dfs(int index, int prev, vector<int>&nums, int n, vector<vector<int>>&dp){
+        if(index == n) return 0;
+        if(dp[index][prev+1] != -1)return dp[index][prev+1];
+        int notTake = dfs(index+1, prev, nums, n, dp);
+        int take = 0;
+        if(prev == -1 || nums[index] > nums[prev]){
+            take += 1 + dfs(index+1, index, nums, n, dp);
+        }
+        return dp[index][prev+1] = max(take, notTake); // prev+1 to avoid -1 index issue
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,1);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i], dp[j]+1);
-                }
-            }
-        }
-        return *max_element(dp.begin(), dp.end());
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return dfs(0,-1,nums,n,dp);
     }
 };
