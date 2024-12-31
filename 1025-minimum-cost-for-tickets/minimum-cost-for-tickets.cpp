ef253367232cad31403607f037f574aacb0fcf7f
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2024-12-31 18:20:41 +0530

    Time: 1 ms (36.20%) | Memory: 14.5 MB (9.21%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    int minCost_ticketHelper(vector<int> &days, vector<int> &costs, int i, vector<int> &dp)
-    {
-        // base case
-        dp[days.size()] = 0;
+    int helper(int i, vector<int>& days, vector<int>& cost, vector<int>&dp){
+        if(i >= days.size()) return 0;
+        if(dp[i] != -1)return dp[i];
+        //for one day
+        int cost1 = cost[0] + helper(i+1, days, cost, dp);
+        //for 7 days
+        int index = i;
+        int till = days[i] + 6;
+        while(index < days.size() && days[index] <= till) index++;
+        int cost2 = cost[1] + helper(index, days, cost, dp);
+        //for one month
+        index = i;
+        till = days[i] + 29;
+        while(index < days.size() && days[index] <= till) index++;
+        int cost3 = cost[2] + helper(index, days, cost, dp);
 
-        // move opposite to top down
-        for (int i = days.size() - 1; i >= 0; i--)
-        {
-            int cost1 = costs[0] + dp[i + 1];
-
-            // 7 days pass taken
-            int passEndDay = days[i] + 7 - 1;
-            int j = i;
-            while (j < days.size() && days[j] <= passEndDay)
-            {
-                j++;
-            }
-            int cost7 = costs[1] + dp[j];
-
-            // 30 days pass taken
-            passEndDay = days[i] + 30 - 1;
-            j = i;
-            while (j < days.size() && days[j] <= passEndDay)
-            {
-                j++;
-            }
-            int cost30 = costs[2] + dp[j];
-            // 2.store the answer
-            dp[i] = min(cost1, min(cost7, cost30));
-        }
-        return dp[0];
+        return dp[i] = min({cost1, cost2, cost3});
     }
 
-    int mincostTickets(vector<int> &days, vector<int> &costs)
-    {
-        // 1.create dp array
-        vector<int> dp(days.size() + 1, -1); // because i is varying and that relies on days array
-        return minCost_ticketHelper(days, costs, 0, dp);
+    int mincostTickets(vector<int>& days, vector<int>& costs){
+        vector<int>dp(days.size(), -1);
+        return helper(0, days, costs, dp);
     }
 };
