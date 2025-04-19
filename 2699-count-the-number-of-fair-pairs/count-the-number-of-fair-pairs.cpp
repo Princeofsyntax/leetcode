d9f7130c649af8870277798f42362856b0086a8c
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2025-04-19 16:45:57 +0530

    Time: 59 ms (46.51%) | Memory: 60.5 MB (38.74%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -2,40 +2,25 @@ class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         long long ans = 0;
-        // since we have to only count pairs, so we can sort
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-
-        for(int i = 0; i < n-1; i++){
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
             int left = i+1;
             int right = n-1;
             while(left <= right){
                 int mid = left + (right-left)/2;
-                if(nums[i]+nums[mid] >= lower){
-                    right = mid-1;
-                }
-                else{
-                    left = mid+1;
-                }
+                if(nums[i]+nums[mid] >= lower)right = mid-1;
+                else left = mid+1;
             }
             int start = left;
             right = n-1;
-            
             while(left <= right){
                 int mid = left + (right-left)/2;
-                if(nums[i]+nums[mid] <= upper){
-                    left = mid+1;
-                }
-                else{
-                    right = mid-1;
-                }
-            }
+                if(nums[i]+nums[mid] <= upper) left = mid+1;
+                else right = mid-1;
+            } 
             int end = right;
-
-            if(start <= end){
-                ans += (end-start+1);
-            }
-
+            if(start<=end) ans += (end-start+1);
         }
         return ans;
     }
