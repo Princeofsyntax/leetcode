3e074ff799fb5201a4caa9184ad990149bbe036b
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2025-05-08 23:22:40 +0530

    Time: 220 ms (81.65%) | Memory: 94.9 MB (93.25%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = 0;
+
+        //use tuple instead of array: (time, row, col)
+        using T = tuple<int, int, int>;
+        priority_queue<T, vector<T>, greater<T>> pq;
+        pq.push({0, 0, 0});
+
+        int dirs[5] = {-1, 0, 1, 0, -1};
+
+        while (!pq.empty()) {
+            auto [d, i, j] = pq.top();
+            pq.pop();
+
+            if (i == n - 1 && j == m - 1) return d;
+            if (d > dist[i][j]) continue;
+            
+            for (int k = 0; k < 4; ++k) {
+                int x = i + dirs[k];
+                int y = j + dirs[k + 1];
+
+                if (x >= 0 && x < n && y >= 0 && y < m) {
+                    int t = max(moveTime[x][y], dist[i][j]) + (i + j) % 2 + 1;
+                    if (dist[x][y] > t) {
+                        dist[x][y] = t;
+                        pq.push({t, x, y});
+                    }
+                }
+            }
+        }
+        return -1; 
+    }
+};
