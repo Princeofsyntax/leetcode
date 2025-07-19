2ef43da4f5d282e8df840712351544b6576b2606
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2025-07-19 10:27:22 +0530

    Time: 47 ms (84.09%) | Memory: 47.4 MB (84.95%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        vector<string> ans;
-        
-        string prev = "";  
-        for (const auto& f : folder) {
-            if (prev.empty() || f.substr(0, prev.size()) != prev || f[prev.size()] != '/'){
-                ans.push_back(f);
-                prev = f;  
+        int n = folder.size();
+        sort(folder.begin(), folder.end()); //sorting lexicographically
+        vector<string>result;
+        result.push_back(folder[0]);
+        for (int i = 1; i < folder.size(); ++i) {
+            const string& parent = result.back();
+            const string& current = folder[i];
+            string parent_prefix = parent + "/";
+                
+            //The string::rfind(prefix, 0) == 0 is a C++ idiom to check for a prefix.
+            if (current.rfind(parent_prefix, 0) != 0) {
+                result.push_back(current);
             }
         }
-        
-        return ans;
+        return result;
     }
 };
