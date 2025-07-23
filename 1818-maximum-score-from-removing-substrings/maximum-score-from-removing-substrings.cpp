d47b33acbd74b9a0f71d0e2caf6d645162d4642d
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2025-07-23 16:41:53 +0530

    Time: 35 ms (72.16%) | Memory: 27.2 MB (69.59%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
-        if (s.size() < 2) return 0;
-
-        int score = 0;
-
-        // Helper function to remove all instances of the target substring
-        // and accumulate the points.
-        auto removeSubstr = [&](string &s, string target, int points) {
-            stack<char> st;
-            for (char ch : s) {
-                if (!st.empty() && st.top() == target[0] && ch == target[1]) {
-                    st.pop();
-                    score += points;
-                } else {
-                    st.push(ch);
-                }
+    int removePair(string &s, char first, char second, int points) {
+        int total = 0;
+        string temp;
+        for (char c : s) {
+            if (!temp.empty() && temp.back() == first && c == second) {
+                temp.pop_back();
+                total += points;
+            } else {
+                temp.push_back(c);
             }
-
-            string remaining;
-            while (!st.empty()) {
-                remaining += st.top();
-                st.pop();
-            }
-            reverse(remaining.begin(), remaining.end());
-            return remaining;
-        };
-
-        // First, prioritize the higher points operation
-        if (x > y) {
-            s = removeSubstr(s, "ab", x);
-            s = removeSubstr(s, "ba", y);
-        } else {
-            s = removeSubstr(s, "ba", y);
-            s = removeSubstr(s, "ab", x);
         }
+        s = temp;
+        return total;
+    }
 
-        return score;
+    int maximumGain(string s, int x, int y) {
+        int total = 0;
+        if (x >= y) {
+            total += removePair(s, 'a', 'b', x);
+            total += removePair(s, 'b', 'a', y);
+        } else {
+            total += removePair(s, 'b', 'a', y);
+            total += removePair(s, 'a', 'b', x);
+        }
+        return total;
     }
 };
