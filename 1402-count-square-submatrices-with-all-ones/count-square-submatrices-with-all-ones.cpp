f59cd9e7dc1b310d9da662f55f5b25a0f56ce8b8
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2025-08-20 11:22:14 +0530

    Time: 19 ms (10.22%) | Memory: 31.6 MB (6.35%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,30 +1,41 @@
 class Solution {
+private:
+    vector<vector<int>> memo;
+
+    int solve(int i, int j, const vector<vector<int>>& matrix) {
+        if (i < 0 || j < 0 || matrix[i][j] == 0) {
+            return 0;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        int top = solve(i - 1, j, matrix);
+        int left = solve(i, j - 1, matrix);
+        int topLeft = solve(i - 1, j - 1, matrix);
+
+        return memo[i][j] = 1 + min({top, left, topLeft});
+    }
+
 public:
-    int countSquaresHelper(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-    if (i < 0 || j < 0) return 0; 
-    if (matrix[i][j] == 0) return 0; 
-    if (dp[i][j] != -1) return dp[i][j];
-
-    dp[i][j] = min({countSquaresHelper(i - 1, j, matrix, dp),
-                    countSquaresHelper(i, j - 1, matrix, dp),
-                    countSquaresHelper(i - 1, j - 1, matrix, dp)}) + 1;
-
-    return dp[i][j];
-}
-
-
     int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int totalSquares = 0;
+        if (matrix.empty() || matrix[0].empty()) {
+            return 0;
+        }
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                totalSquares += countSquaresHelper(i, j, matrix, dp);
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        memo.assign(n, vector<int>(m, -1));
+
+        int totalCount = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                totalCount += solve(i, j, matrix);
             }
         }
 
-        return totalSquares;
+        return totalCount;
     }
 };
