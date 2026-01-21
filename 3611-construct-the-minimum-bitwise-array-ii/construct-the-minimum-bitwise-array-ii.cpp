00be025128b7a7d17bf8832a606c42b7b1df7ede
Prince Upadhyay <142150438+Princeofsyntax@users.noreply.github.com>
2026-01-21 21:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (31.07%) - LeetSync

--- 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        for (int& p: nums) {
-            int remB=(1<<countr_one((unsigned)p))>>1;
-            p^=(remB)|(-(remB==0) & ~p);
+        vector<int> sol;
+        for(auto x : nums){
+            if(x % 2 == 0){
+                sol.push_back(-1);
+            }
+            else{
+                int t = 0;
+                while(x & (1 << t)) t ++;
+                sol.push_back(x ^ (1 << (t - 1)));
+            }
         }
-        return move(nums);
+        return sol;
     }
 };
